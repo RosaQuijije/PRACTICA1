6a416387d397391daf0503e7aeadfe4b17a53382
RosaQuijije <elisab17@hotmail.com>
2024-07-06 16:32:29 -0500

    PRACTICA1
    PRACTICA EN CLASE

---
+++ PRACTICAS1/P1/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',funtion(){
+    var.boton = document.getElementById('miBoton):
+    boton.addEventListener('click',cambiarContenido);
+
+});
+
+function cambiarContenido(){
+    var parrafo=document.getElementById('Parrafo');
+    parrafo. innerHTML="El contenido ha sido cambiado";
+    parrafo.style.color="red"
+}
